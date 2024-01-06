f9da935712964a07ba10fdc6bbbe93bb315c5237
HaNAst.L <limlexia@gmail.com>
2024-01-06 23:06:58 +0900

    add humor-pangi.reviewtoday.net

--- block-coupang.js
+++ block-coupang.js
@@ -2,19 +2,29 @@
 // @name block coupang article ad
 // @name:ko 쿠팡 납치글 무효화
 // @description:ko 웹 페이지에서 쿠팡 페이지를 봐야만 컨텐츠를 표시하는 사이트를 무효화합니다.
-// @version 0.0.1
+// @version 0.0.2
 // @match https://*/*
 // @match https://ggul.zal.kr/*
+// @match https://humor-pangi.reviewtoday.net/*
 
 // @run-at document-end
+// @run-at document-idle
+
 // @downloadURL  https://raw.githubusercontent.com/wwwhana/block-ads/main/block-coupang.js
 // @updateURL https://raw.githubusercontent.com/wwwhana/block-ads/main/block-coupang.js
 // ==/UserScript==
 
 (function(){
     "use strict";
-    const v = document.getElementById("bo_v_atc") || document.getElementById("inner-content-body") ;
+    let v = document.getElementById("bo_v_atc") || document.getElementById("inner-content-body") ;
     v && (() => { v.classList.remove("omit_wrap"); 
     v.classList.remove("hided");
     v.style.maxHeight = "unset" })();
+
+	let vx = document.getElementsByClassName("post-contents") && document.getElementsByClassName("post-contents")[0];
+	vx && vx.children  && vx.children[0] && vx.children[0] && (() => {
+	console.log("Z");
+	vx.children[0].style.height = "unset" ;
+	vx.children[0].style.overflow = "unset";
+	})(); 
 })();
